cb0797fb4e2deb1f0084d24c9605dd6773e5e93c
SilentWitchy <amonthofaugust@gmail.com>
2025-12-30 03:32:25 -0600

    Add settings menu and main menu escape exit

--- src/ui/Ui.h
+++ src/ui/Ui.h
@@ -14,7 +14,12 @@ public:
     void MainMenuRender(Renderer& r);
     int  MainMenuSelection() const { return m_mainMenuSelection; }
     bool MainMenuActivated() const { return m_mainMenuActivated; }
-    void ClearMainMenuActivated(); 
+    void ClearMainMenuActivated();
+
+    void SettingsTick(bool upPressed, bool downPressed, bool selectPressed, bool backPressed);
+    void SettingsRender(Renderer& r);
+    bool SettingsBackRequested() const { return m_settingsBackRequested; }
+    void ClearSettingsBackRequest();
 
     void WorldGenTick(bool upPressed, bool downPressed, bool leftPressed, bool rightPressed,
         bool selectPressed, bool backPressed);
@@ -32,8 +37,13 @@ private:
     Font& m_font;
 
     // Main menu state
-    int  m_mainMenuSelection = 0; // 0 = New World, 1 = Quit
+    int  m_mainMenuSelection = 0; // 0 = New World, 1 = Settings, 2 = Quit
     bool m_mainMenuActivated = false;
+
+    // Settings menu state
+    int  m_settingsSelection = 0;
+    bool m_settingsBackRequested = false;
+    std::string m_settingsDetail;
 
     int m_wgRow = 0;                 // which of the 7 options (0..6)
     int m_wgChoice[7] = { 2,2,2,2,2,2,2 }; // default to "Middle" option
